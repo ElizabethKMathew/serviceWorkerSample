7ffad6bdc7525dd07a7e4e0a3295928ad7373537
elizabeth <elizabeth@qburst.com>
2018-04-11 18:02:11 +0530

    UI styles for page

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,35 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: './src/js/index.js'
+  entry: './src/js/index.js',
+  module: {
+        rules: [{
+            test: /\.(scss|css)$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/,
+          loader: 'url-loader'
+        },
+        {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        },
+        {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: "file-loader"
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      })
+    ]
 }
