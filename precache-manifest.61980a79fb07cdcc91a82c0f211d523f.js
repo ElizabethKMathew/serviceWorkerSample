e2f64e7d72be7ddb7315314ba0c9fcd106001e6f
elizabeth <elizabeth@qburst.com>
2018-04-12 12:20:11 +0530

    deploying to gh-pages

---
+++ precache-manifest.61980a79fb07cdcc91a82c0f211d523f.js
@@ -0,0 +1,28 @@
+self.__precacheManifest = [
+  {
+    "revision": "842eb42b147a076e73f7",
+    "url": "main.js"
+  },
+  {
+    "revision": "3d764216a531ed6b87d6fc5de91a7196",
+    "url": "index.html"
+  },
+  {
+    "url": "b180c799a87a4cad9108713ebe309686.eot"
+  },
+  {
+    "url": "629a55a7e793da068dc580d184cc0e31.ttf"
+  },
+  {
+    "url": "50145685042b4df07a1fd19957275b81.ttf"
+  },
+  {
+    "url": "33696c1691b434f58dd2887f2fa2a30f.eot"
+  },
+  {
+    "url": "1bf71be111189e76987a4bb9b3115cb7.ttf"
+  },
+  {
+    "url": "06a04537db9294681f93bc12bd1d9c53.eot"
+  }
+];
